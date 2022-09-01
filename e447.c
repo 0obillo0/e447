55cdc564dd3b4ba6d9bc5c970ebec9aebc46cd98
0obilo0 <bill920510888@gmail.com>
2022-09-01 15:04:15 +0800

    second

--- e447.c
+++ e447.c
@@ -1,7 +1,33 @@
 #include<stdio.h>
 
+int a[10000] = {0};
+
 int main()
 {
+	int n, k, now = -1, head = 0;
+	scanf("%d", &n);
+	for(int i = 0; i <n; i++)
+	{
+		scanf("%d", &k);
+		if(k ==1)
+		{
+			now++;
+			scanf("%d", &a[now]);
+		}else if(k == 2)
+		{
+			if(head - 1 == now)
+				printf("-1\n");
+			else
+				printf("%d\n", a[head]);
+		}else
+		{
+			if(head - 1 != now)
+			{
+				a[head] = 0;
+				head++;
+			}
+		}
+	}	
 	
 
 	return 0;	
